adbff896f74af62a9df00aa0a7e609cb12a38b6d
Dubjay18 <jejeniyi7@gmail.com>
2024-08-06 23:06:50 +0100

    init search input

--- app/main/course.tsx
+++ app/main/course.tsx
@@ -1,10 +1,16 @@
 import { RegularText } from "@/components/StyledText";
-import { View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { TextInput, View } from "react-native";
 
 export default function Course() {
 	return (
 		<View>
-			<RegularText>Course</RegularText>
+			<View className="flex-row">
+				<AntDesign name="search1" size={24} color="black" />
+				<TextInput placeholder="Search" />
+			</View>
+			<RegularText>Coure</RegularText>
 		</View>
 	);
 }
+
